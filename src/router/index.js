a0652eb8d115f1dddc22bce69dffafa7cf514e59
chinoroku <angelocaveri@gmail.com>
2023-12-27 01:02:19 -0500

    tienda inicio

--- src/router/index.js
+++ src/router/index.js
@@ -11,15 +11,12 @@ const routes = [
     component: HomeView
   },
   {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: function () {
-      return import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-    }
+    path: '/login',name:'login',component: ()=> import('@/views/LoginApp.vue'),
+  },
+  {
+    path: '/shop',name:'shop',component: ()=> import('@/views/productos/IndexProductoApp.vue'),
   }
+ 
 ]
 
 const router = new VueRouter({
